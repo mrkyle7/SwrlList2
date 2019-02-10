5dabcb8e2d3be62ead4ce172002bcfa07ece3c66
Kyle Harrison <kylejharrison@gmail.com>
2019-02-10 11:35:06 +0000

    add static health endpoint to test deploy

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.static('platforms/browser/www'));
 app.set('port', process.env.PORT || 8000);
+app.get('/api/v1/health', function (req, res){
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({isAvailable: true}));
+  console.log('Deployed successfully');
+})
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
